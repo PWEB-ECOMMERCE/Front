8fb5e1cdde4bf560dc48c84a5a61aa4f7a12ed5a
Dante de Araujo <danteeng@hotmail.com>
2024-04-12 16:45:57 -0300

    feat(auth): add auth provider to global layout and start auth implementation

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,17 @@
-// -import './global.css'
-import React from 'react'
-import { Inter } from 'next/font/google'
-import ThemeRegistry from '@/utils/ThemeRegistry'
+import React from 'react';
+import { Inter } from 'next/font/google';
+import ThemeRegistry from '@/utils/ThemeRegistry';
+import { Providers } from './providers';
 
 const inter = Inter({subsets: [ 'latin' ]})
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
   return (
     <html lang="en">
         <body className={inter.className}>
           <ThemeRegistry options={{key:'mui-theme'}}>
+            <Providers>
               {children}
+            </Providers>
           </ThemeRegistry>
         </body>
     </html>
---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect, createContext } from "react";
+
+export const AuthContext = createContext({});
+
+export function AuthProvider({children}){
+  const [user, setUser] = useState(null);
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+
+  }, [])
+
+  async function signIn({email,password}){
+
+    const data = await fetch("http://localhost:8080/usuarios")
+    const json = await data.json();
+    console.log(json);
+
+    setUser({
+      nome:"Dante",
+      email:"danteeng@hotmail.com",
+
+    })
+  }
+
+  return (
+    <AuthContext.Provider value={{user, isAuthenticated, signIn}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
