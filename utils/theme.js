d4c455531011d34bb8828621e3273365d0ab9b1f
Dante de Araújo <danteeng@hotmail.com>
2024-04-15 10:34:05 -0300

    Revert "Dockerizing application"

---
+++ utils/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+export default createTheme({
+ palette: {
+   primary: {
+     main: "#a0a0a0",
+   },
+   dante: {
+     main: "#ff00ff",
+     dark: "#ffff00"
+   }
+ },
+});
